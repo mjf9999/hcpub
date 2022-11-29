4ac5a6e8e24e96d4244859eb9a0872bbc7dd6949
morton <727913417@qq.com>
2022-11-29 10:24:57 +0800

    fix:modify params by Create

--- src/order/Create.php
+++ src/order/Create.php
@@ -22,7 +22,31 @@ class Create extends Base
                 "bookName" => "书名",
                 "totalNumber" => 10,
                 "finishSize" => "210*280",
+                "bindingTypeCode" => 1,
                 "indexNo" => 1,
+                "bookFileList" => [
+                    [
+                        "partType" => "C",
+                        "fileName" => "tangshuibook-11110-cover.pdf",
+                        "fileUrl" => "https://oss-fs.bbys.cn/mall/upload/20221116/5badd0fa04cca88f098aa003ad4ce177.pdf",
+                        "colorCode" => 2,
+                        "materialCode" => 1
+                    ],
+                    [
+                        "partType" => "P",
+                        "fileName" => "tangshuibook-11110-inner.pdf",
+                        "fileUrl" => "https://oss-fs.bbys.cn/mall/upload/20221117/643791013547b4209fb7bc33207bdf8b.pdf",
+                        "colorCode" => 1,
+                        "materialCode" => 1
+                    ]
+                ]
+            ],
+            [
+                "bookName" => "书名",
+                "totalNumber" => 10,
+                "finishSize" => "210*280",
+                "bindingTypeCode" => 1,
+                "indexNo" =>4,
                 "bookFileList" => [
                     [
                         "partType" => "C",
@@ -38,16 +62,16 @@ class Create extends Base
             ]
         ],
         "receiveInfos" => [
-            "detailedAddress" => "测试地址520号",
+            "detailedAddress" => "北京市北京东城区平湖街道地址520号",
             "contactMan" => "默默",
             "contactMobile" => "18877772222",
             "province" => "北京市",
-            "city" => "广州",
+            "city" => "北京市",
             "county" => "东城区",
             "streetAddress" => ""
         ],
         "callBackUrl" => "https://mall.bbys.cn/api/index/callback",
-        "outOrderNo" => "12345677894"
+        "outOrderNo" => "12345677814"
     ];
 
     protected function validate()
